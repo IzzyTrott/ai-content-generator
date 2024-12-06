0ad79f042ebad6da26688afc8aeca2811cc47b4b
IzzyTrott <isabeltrottphotography@gmail.com>
2024-12-06 14:00:33 +0000

    Added basic JS funcationality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateContent(event) {
+  event.preventDefault();
 
+  new Typewriter("#content", {
+    strings: "content tbc",
+    autoStart: true,
+    delay: 1,
+    cursor: null,
+  });
+}
+
+let inputElement = document.querySelector("#content-generator");
+inputElement.addEventListener("submit", generateContent);
