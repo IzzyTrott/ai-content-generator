1e2c142d47d714cbcd204c9c08d03259ea082ef3
IzzyTrott <isabeltrottphotography@gmail.com>
2024-12-06 17:30:39 +0000

    Updated JS to included options for poem, joke and quote

--- src/index.js
+++ src/index.js
@@ -10,14 +10,15 @@ function displayContent(response) {
 function generateContent(event) {
   event.preventDefault();
   let textInput = document.querySelector("#text-input");
+  let contentOption = document.querySelector("#select-content");
+
   let content = document.querySelector("#content");
   content.innerHTML = `<span class="blink">⏳ Generating your content about ${textInput.value}</span>`;
   content.classList.remove("hidden");
 
   let apiKey = "475c1b09tc9cf784eac1636c9abbbco2";
-  let context =
-    "You are an inspirational poem writer who loves to write short poems. Generate a 4 line poem in basic HTML and separate each senence with <br/>. Do not show '```html' or the title of the poem. Make sure to follow the user instructions. At the bottom of the poem, sign it 'SheCodes AI' in a <strong>";
-  let prompt = `User instructions: Generate a poem about ${textInput.value}`;
+  let context = `You are an inspirational ${contentOption.value} writer who loves to write short ${contentOption.value}. Generate a 2, 3 or 4 line ${contentOption.value} in basic HTML and separate each sentence with <br/>. Do not show the title of the ${contentOption.value}. Make sure to follow the user instructions. At the bottom, sign it 'SheCodes AI' in a <strong>`;
+  let prompt = `User instructions: Generate a ${contentOption.value} about ${textInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayContent);
