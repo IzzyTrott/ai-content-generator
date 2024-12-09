f0f01839bf57616138d6cb10ba66353d825088c5
IzzyTrott <isabeltrottphotography@gmail.com>
2024-12-09 16:16:07 +0000

    added custom font, icons and updates css

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function generateContent(event) {
   content.classList.remove("hidden");
 
   let apiKey = "475c1b09tc9cf784eac1636c9abbbco2";
-  let context = `You are an inspirational ${contentOption.value} writer who loves to write short ${contentOption.value}. Generate a 2, 3 or 4 line ${contentOption.value} in basic HTML and separate each sentence with <br/>. Do not show the title of the ${contentOption.value}. Make sure to follow the user instructions. At the bottom, sign it 'SheCodes AI' in a <strong>`;
+  let context = `You are an inspirational ${contentOption.value} writer who loves to write simple ${contentOption.value}. Generate a very short ${contentOption.value} in basic HTML and separate each sentence with <br/>. Do not show the title of the ${contentOption.value}. Make sure to follow the user instructions. At the bottom, sign it 'SheCodes AI' in a <strong>`;
   let prompt = `User instructions: Generate a ${contentOption.value} about ${textInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
